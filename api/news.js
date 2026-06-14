0dc72ee1da82482716e03189fc7146aeabadfca8
mattmonahan2 <53827704+mattmonahan2@users.noreply.github.com>
2026-06-14 16:11:16 -0400

    fix: increase NewsAPI pageSize to 10

--- api/news.js
+++ api/news.js
@@ -3,7 +3,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET');
   const q = req.query.q || 'business careers jobs';
   const apiKey = 'e7c75424d2644146962b02e145f34590';
-  const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(q)}&language=en&sortBy=publishedAt&pageSize=5&apiKey=${apiKey}`;
+  const url = `https://newsapi.org/v2/everything?q=${encodeURIComponent(q)}&language=en&sortBy=publishedAt&pageSize=10&apiKey=${apiKey}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
